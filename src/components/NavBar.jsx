8c5ca3028f867da8c54a5e8f60ffff4a8a5592ff
AlyssaD <pink_lime@live.com>
2022-10-14 21:07:09 -0400

    set up navBar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,33 @@ import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <>
-    </>
+    <nav>
+      <Link to='/'>
+        Alyssa DeM
+      </Link>
+      <ul>
+        <li>
+          <Link to='about'>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='resume'>
+            Resume
+          </Link>
+        </li>
+        <li>
+          <Link to='projects'>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to='contact'>
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
