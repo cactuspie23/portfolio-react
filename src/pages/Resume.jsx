d28397707bb0349b0e6947066f09a83689e0d21b
AlyssaD <pink_lime@live.com>
2022-11-06 19:43:36 -0500

    add resume for download

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,6 +2,8 @@ const Resume = () => {
   return (
     <>
       <h1>Resume</h1>
+      <p>Get a copy of my resume here.</p>
+      <a href='/PublicAlyssaDeMartinisResume.pdf' download>Download</a>
     </>
   )
 }
