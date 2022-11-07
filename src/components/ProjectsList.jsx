a18c3908e98c2c1c0c23a010e9acad5193924072
AlyssaD <pink_lime@live.com>
2022-11-06 19:01:32 -0500

    update index and about section. Add projectlist

---
+++ src/components/ProjectsList.jsx
@@ -0,0 +1,18 @@
+import { projects } from "../data/projects"
+
+const Projects = () => {
+  return (
+    <>
+      <h1>Projects</h1>
+      <ul>
+        {projects.map(project => 
+          <li>
+            {project.title}
+          </li>  
+        )}
+      </ul>
+    </>
+  )
+}
+
+export default Projects
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ const About = () => {
     <>
       <h1>About</h1>
       <img src='images/profile2.jpg' alt='Alyssa' />
+      <p>I’m a full-stack software developer with a background in data entry and customer service. Through my data entry experience I've acquired exceptional attention to detail. My unique job history has taught me to be adaptable in any work environment. I possess a natural curiosity to understand how things work, which has compelled me to be self-motivated and autodidactic.</p>
     </>
   )
 }
