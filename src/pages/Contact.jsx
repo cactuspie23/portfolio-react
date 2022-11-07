e5fb242b70f61145cd0497e400a89d724d2de8ca
AlyssaD <pink_lime@live.com>
2022-11-06 20:34:04 -0500

    update contact images and info

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,16 @@ const Contact = () => {
   return (
     <>
       <h1>Contact</h1>
+      <p>Reach out if you'd like to collaberate!</p>
+      <a href="mailto:alyssadee@outlook.com">
+        <img src='images/email.png' alt='email' />
+      </a>
+      <a href="https://github.com/cactuspie23">
+        <img src='images/github.png' alt='github' />
+      </a>
+      <a href="https://www.linkedin.com/in/alyssa-demartinis/">
+        <img src='images/linkedin.png' alt='linkedin' />
+      </a>
     </>
   )
 }
