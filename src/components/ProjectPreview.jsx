c39275b17c54f15b2f293c28140bc470f76b1121
AlyssaD <pink_lime@live.com>
2022-11-11 14:45:53 -0500

    update project images and projects page styling

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,15 +1,16 @@
 import hyphenateWords from "../utilities/hyphenateWords.js"
 import { Link } from "react-router-dom"
+import styles from './ProjectPreview.module.css'
 import Card from 'react-bootstrap/Card'
 
 const ProjectPreview = ({title, image}) => {
   const path = hyphenateWords(title)
   return(
-    <Card style={{ width: '18rem', backgroundColor: '#2c2c3a' }}>
+    <Card className={styles.card}>
       <Link to={path} style={{ textDecoration: 'none' }}>
-        <Card.Img variant="top" src={image} alt={title} />
+        <Card.Img variant="top" src={image} alt={title} className={styles.cardImg}/>
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
+          <Card.Title className={styles.cardTitle}>{title}</Card.Title>
         </Card.Body>
       </Link>
     </Card>
