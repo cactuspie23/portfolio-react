ff99b44b5c46f3e6c460ab4db11b51e82b415ece
AlyssaD <pink_lime@live.com>
2022-11-11 12:10:01 -0500

    add bootstrap

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,16 +1,18 @@
 import hyphenateWords from "../utilities/hyphenateWords.js"
-import styles from './ProjectPreview.module.css'
 import { Link } from "react-router-dom"
+import Card from 'react-bootstrap/Card'
 
 const ProjectPreview = ({title, image}) => {
   const path = hyphenateWords(title)
   return(
-    <Link to={path}>
-      <div className={styles.projectContainer}>
-        <img src={image} alt={title}/>
-        <h3>{title}</h3>
-      </div>
-    </Link>
+    <Card style={{ width: '18rem', backgroundColor: '#2c2c3a' }}>
+      <Link to={path} style={{ textDecoration: 'none' }}>
+        <Card.Img variant="top" src={image} alt={title} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+        </Card.Body>
+      </Link>
+    </Card>
   )
 }
 
--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -1,9 +1,10 @@
 import ProjectPreview from "./ProjectPreview"
+import styles from './ProjectList.module.css'
 
 const ProjectsList = ({projects}) => {
   return (
     <>
-      <section>
+      <section className={styles.projectContainer}>
         {projects.map((project, i) => 
           <ProjectPreview key={i} title={project.title} image={project.image} />
         )}
