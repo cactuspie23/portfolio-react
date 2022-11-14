6c4f04dbeedffdcdb04f9f85836898b15051d0cd
AlyssaD <pink_lime@live.com>
2022-11-14 15:25:25 -0500

    update contact

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,23 @@
+import styles from './Contact.module.css'
+
+const Contact = () => {
+  return (
+    <>
+      <h1>Contact</h1>
+      <p>Reach out if you'd like to collaberate!</p>
+      <div className={styles.contacts}>
+        <a href="mailto:alyssadee@outlook.com">
+          <img src='images/email.png' alt='email' />
+        </a>
+        <a href="https://github.com/cactuspie23">
+          <img src='images/github.png' alt='github' />
+        </a>
+        <a href="https://www.linkedin.com/in/alyssa-demartinis/">
+          <img src='images/linkedin.png' alt='linkedin' />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default Contact
