da36fceccf8a61f1b347b8121087868f62c3889f
AlyssaD <pink_lime@live.com>
2022-11-22 16:52:03 -0500

    update resume download

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,17 +1,23 @@
+import styles from './ProjectDetails/ProjectDetails.module.css'
+
 const Resume = () => {
   return (
     <>
       <h1>Resume</h1>
       <p>Get a copy of my resume here.</p>
+      <div className={styles.btns}>
+        <a href="/AlyssaDeMartinisResume.pdf" download>
+          <button>Download</button>
+        </a>
+      </div>
       <iframe
         src="/AlyssaDeMartinisResume.pdf"
         title="resume"
         frameBorder="0"
         scrolling="auto"
-        height="80%"
-        width="60%"
+        height="65%"
+        width="65%"
       ></iframe>
-
     </>
   )
 }
