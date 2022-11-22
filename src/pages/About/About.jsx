4454b7401d86157c1ae798d4ac6c3a7e1c0da16c
AlyssaD <pink_lime@live.com>
2022-11-22 15:16:47 -0500

    update about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,6 +8,8 @@ const About = () => {
         <img src='images/profile2.jpg' alt='Alyssa' />
         <div>
           <p>I’m a full-stack software developer with a background in data entry and customer service. Through my data entry experience I've acquired exceptional attention to detail. My unique job history has taught me to be adaptable in any work environment. I possess a natural curiosity to understand how things work, which has compelled me to be self-motivated and autodidactic.</p>
+          <h3>Skills:</h3>
+          <h5>JavaScript, Python, React, Node, Express, Mongoose, MongoDB, PostgreSQL, Django</h5>
         </div>
       </div>
     </>
